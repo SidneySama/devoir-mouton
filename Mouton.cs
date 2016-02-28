ecbbf6f257906681eb4919e6a9b2248236e95914
SidneySama <sidney.gyde@laposte.net>
2016-02-28 23:28:50 +0100

    Sheep&Hell

---
+++ Mouton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouton : MonoBehaviour {
+	private Animator mouton;
+
+
+
+	void Start () {
+
+		mouton = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey ("up")) {
+			mouton.SetBool ("up", true);
+		} else {
+			mouton.SetBool ("up", false);
+		}
+		if (Input.GetKey ("left")) {
+			mouton.SetBool ("left", true);
+		} else {
+			mouton.SetBool ("left", false);
+		}
+		if (Input.GetKey ("right")) {
+			mouton.SetBool ("right", true);
+		} else {
+			mouton.SetBool ("right", false);
+		}
+		if (Input.GetKey ("down")) {
+			mouton.SetBool ("down", true);
+		} else {
+			mouton.SetBool ("down", false);
+		}
+	}
+}
---
+++ exit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exit: MonoBehaviour {
+	void Update ()
+	{
+		if (Input.GetKey ("escape"))
+			Application.Quit ();
+
+	}
+}
